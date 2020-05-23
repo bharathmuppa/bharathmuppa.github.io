4e15162d1456fc342dbafc25d3591324dc5728d9
bharathmuppa <bharathmuppa@gmail.com>
2020-05-23 22:51:18 +0200

    added fontawesome for build setup

--- src/views/footer/footer.component.js
+++ src/views/footer/footer.component.js
@@ -61,25 +61,25 @@ export class Footer extends LitElement {
       <div class="footer flex-center">
         <fa-icon
           class="fab fa-google icon-3d"
-          path-prefix="../node_modules"
+          src="../src/assets/brands.svg"
           style="fill: #2980B9;height: 3em;width: 3em;"
           @click="${this.openMail}"
         ></fa-icon>
         <fa-icon
           class="fab fa-linkedin icon-3d"
-          path-prefix="../node_modules"
+          src="../src/assets/brands.svg"
           style="fill: #2980B9;height: 3em;width: 3em; "
           @click="${this.openLinkedin}"
         ></fa-icon>
         <fa-icon
           class="fab fa-twitter icon-3d"
-          path-prefix="../node_modules"
+          src="../src/assets/brands.svg"
           style="fill: #2980B9;height: 3em;width: 3em;"
           @click="${this.openTwitter}"
         ></fa-icon>
         <fa-icon
           class="fab fa-dev icon-3d"
-          path-prefix="../node_modules"
+          src="../src/assets/brands.svg"
           style="fill: #2980B9;height: 3em;width: 3em;"
           @click="${this.openArticles}"
         ></fa-icon>
